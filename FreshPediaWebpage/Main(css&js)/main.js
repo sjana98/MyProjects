5d4af57f548403268bddfaa3d8582e151573082f
sjana98 <sjana8901@gmail.com>
2023-06-18 19:59:49 +0530

    Update main.js

--- FreshPediaWebpage/Main(css&js)/main.js
+++ FreshPediaWebpage/Main(css&js)/main.js
@@ -1,4 +1,4 @@
-// Header Scroll
+// nav stick
 let nav = document.querySelector('.navbar');
 window.onscroll = function () {
     if (document.documentElement.scrollTop > 50) {
@@ -6,13 +6,24 @@ window.onscroll = function () {
     } else {
         nav.classList.remove('header-scrolled');
     }
-}
-// nav hide
+};
+// nav hide on refresh or new page loading
 let navBar = document.querySelectorAll('nav-link');
 let navCollapse = document.querySelector('.navbar-collapse.collapse');
-navBar.forEach(function (e){
+navBar.forEach(function (e) {
     e.addEventListner('click', function () {
         navCollapse.classList.remove('show');
     })
-})
-
+});
+// nav close by clicking outside of navbar elements
+document.body.addEventListener('click', function (event) {  // Add an event listener for the click event on the document body
+    let isNavbarClicked = event.target.closest('.navbar');  // Check if the clicked element is within the navbar
+    if (!isNavbarClicked) {                                 // If the clicked element is not within the navbar, close the collapse
+        let navbarToggler = document.querySelector('.navbar-toggler');
+        let navbarContent = document.querySelector('.navbar-collapse');
+      
+        if (navbarContent.classList.contains('show')) {
+            navbarToggler.click();
+        }
+    }
+});
